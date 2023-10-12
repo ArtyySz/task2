a7fe4ea67d3386411682c331a1e669f304957380
arty <artgoncarov3@gmail.com>
2023-10-12 20:48:27 +0300

    Посчитал сколько составит переплата и общую сумму выплаты кредита

---
+++ src/task9.js
@@ -0,0 +1,5 @@
+const credit = 200000;
+const overpayment = (9229 * 24) - credit;
+const totalCost = credit + overpayment;
+console.log('Общая сумма выплаты = ' + totalCost);
+console.log('Переплата = ' + overpayment);
